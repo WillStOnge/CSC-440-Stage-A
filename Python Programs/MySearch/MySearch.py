d2d1cd20edcb2275a222186eaec071d5f3222eef
boscha1 <bosch.anthony7@gmail.com>
2021-09-10 14:05:40 -0400

    implemented a mysearch using a linear search algorithm

--- Python Programs/MySearch/MySearch.py
+++ Python Programs/MySearch/MySearch.py
@@ -1,2 +1,13 @@
-def mysearch(input, value):
-    pass
+def my_search(input_file, value):
+    input_list = []
+    f = open(input_file, "r")
+    for line in f:
+        input_list.append(int(line))
+    return linear_search(input_list, value)
+
+
+def linear_search(array_of_values, value):
+    for i in range(len(array_of_values)):
+        if array_of_values[i] == value:
+            return i
+    return -1
