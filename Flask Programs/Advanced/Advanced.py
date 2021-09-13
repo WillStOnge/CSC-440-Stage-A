aab47873fba0eed5583161a499311fba68f4bd67
Will St. Onge <ongewill@gmail.com>
2021-09-13 17:51:20 -0400

    Advanced (#2)
    * Added README
    
    * Added advanced code for WP-11.
    
    * Added the screenshot for Advanced.py
    
    * Made updates to .gitignore
    
    * Added filter tag

---
+++ Flask Programs/Advanced/Advanced.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+import os, random
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    teammate_names = ["Will St. Onge", "Will Sie", "Cory Knoll", "Anthony Bosch", "Justin Gallagher"]
+    return render_template("index.html", app = app, teammate_names = teammate_names, dir = os.getcwd(), display = (random.randint(1, 10) == 1))
+
+if __name__ == "__main__":
+    app.run(debug = True)
