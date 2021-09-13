f489dcfbdd689e09c72c3f42cde54df8359daba9
boscha1 <bosch.anthony7@gmail.com>
2021-09-13 19:45:06 -0400

    Restructured code to raise FileNotFoundError

--- Python Programs/MySearch/MySearch.py
+++ Python Programs/MySearch/MySearch.py
@@ -1,8 +1,17 @@
+"""
+MySearch.py
+Programmer: Anthony Bosch
+Course: CSC440
+"""
+
+import os.path
+
+
 def my_search(input_file, value):
+    if not os.path.isfile(input_file):
+        raise FileNotFoundError
     try:
         with open(input_file, "r") as in_file:
             return [int(i) for i in in_file.read().splitlines()].index(value)
     except ValueError:
         return -1
-    except FileNotFoundError:
-        print("File does not exist")
