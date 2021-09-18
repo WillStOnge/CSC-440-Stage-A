70001e2a943cd72b6762a0b8fc5d631129dcd7f4
Justin <gallagherjust71@gmail.com>
2021-09-18 15:47:20 -0400

    Added more documentation for PyDoc and changed test to check for raised FileNotFoundError

--- Python Programs/MySort/MySortTest.py
+++ Python Programs/MySort/MySortTest.py
@@ -3,6 +3,8 @@ MySortTest.py
 
 Programmer: Justin Gallagher
 Course: CSC440
+
+MySortTest.py contains two unit tests for the method my_sort() in MySort.py
 """
 
 import unittest
@@ -11,17 +13,24 @@ from MySort import my_sort
 
 class MySortTest(unittest.TestCase):
     """
-        Test cases for my_sort()
+        Defines test cases for my_sort()
     """
-
     def test_givenInputFile_returnsSortedList(self):
+        """
+            Given an input file containing integer values, test that the my_sort method
+            correctly returns an ascending, sorted list.
+        """
         test_list = my_sort("input.txt")
         for index in range(len(test_list) - 1):
             self.assertLessEqual(test_list[index], test_list[index + 1],
                                  "List is not in ascending, sorted order in test:test_givenInputFile_returnsSortedList")
 
-    def test_givenNoInputFile_returnsNone(self):
-        self.assertEqual(None, my_sort(""), "Error with input file in test:test_givenNoInputFile_returnsNone")
+    def test_givenNoInputFile_returnsFileNotFoundError(self):
+        """
+            Given no input file, test that the my_sort method correctly raises a FileNotFoundError.
+        """
+        with self.assertRaises(FileNotFoundError):
+            test_list = my_sort("")
 
 
 if __name__ == "__main__":
