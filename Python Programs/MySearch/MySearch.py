83dc064d28090e7eead55f12d9c17b11d1d3aeb9
boscha1 <bosch.anthony7@gmail.com>
2021-09-20 08:32:34 -0400

    Correctly added PyDoc and DocTest

--- Python Programs/MySearch/MySearch.py
+++ Python Programs/MySearch/MySearch.py
@@ -8,6 +8,40 @@ import os.path
 
 
 def my_search(input_file, value):
+    """
+    Search a list of integers for a specific value.
+
+    :param input_file: The file with list of integers
+    :param value:  The value being searched in the list
+    :return: returns the value if found, -1 if not found
+    :raises: FileNotFoundError: Raised if the file cannot be found
+
+    ===========================
+
+    Using ``MySearch``
+    ------------------
+
+    This is an example text file in reStructuredText format.  First import
+    ``my_search`` from the ``MySearch`` module.
+
+    Now use it:
+
+    Given an input file containing integer values and an integer value of 8,
+    the function will return 6, as the value 8 is in the 6th index of the list of values.
+    >>> my_search("input.txt", 8)
+    6
+
+    Given an input file containing integer values and an integer value of 1,
+    the function will return -1, as the value 1 is not in the list of values.
+    >>> my_search("input.txt", 1)
+    -1
+
+    Given an input file that does not exist, a FileNotFoundError will be raised.
+    >>> my_search("", 8)
+    Traceback (most recent call last):
+     ...
+    FileNotFoundError
+    """
     if not os.path.isfile(input_file):
         raise FileNotFoundError
     try:
--- Python Programs/MySearch/MySearchTest.py
+++ Python Programs/MySearch/MySearchTest.py
@@ -12,16 +12,29 @@ class MySearchTest(unittest.TestCase):
     """
         Test cases for my_search()
     """
-
     def test_search_pass(self):
+        """
+            Given an input file containing integer values, and the integer value 8,
+            test that the my_search method correctly returns the index 6 verifying
+            that 8 is in the list.
+        """
         index = my_search("input.txt", 8)
         self.assertEqual(index, 6)
 
     def test_search_fail(self):
+        """
+            Given an input file containing integer values, and the integer value 0,
+            test that the my_search method correctly returns -1 verifying that
+            0 is not in the list.
+        """
         index = my_search("input.txt", 0)
         self.assertEqual(index, -1)
 
     def test_open_file(self):
+        """
+            Given an input file that does not exist, test that the my_search method
+            correctly raises a FileNotFoundError.
+        """
         with self.assertRaises(FileNotFoundError):
             index = my_search("my_file.txt", 8)
 
