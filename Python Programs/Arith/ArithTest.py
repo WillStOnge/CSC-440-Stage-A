67fd196b1c500006f3fade6944c9ad256990f4aa
Will St. Onge <ongewill@gmail.com>
2021-09-20 08:50:17 -0400

    Update ArithTest.py

--- Python Programs/Arith/ArithTest.py
+++ Python Programs/Arith/ArithTest.py
@@ -8,8 +8,17 @@ import unittest
 from Arith import Arith
 
 class ArithTest(unittest.TestCase):
+    """
+    Defines test cases for add() and sub().
+    """
     def test_getSum_twoNumbers(self):
+        """
+        Given 2 integers, the add method should return the correct result.
+        """
         self.assertEqual(17, Arith().add(8, 9))
 
     def test_getDiff_twoNumbers(self):
+        """
+        Given 2 integers, the sub method should return the correct result.
+        """
         self.assertEqual(4, Arith().sub(11, 7))
